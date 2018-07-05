9a4b3999303c8787cd4dada9d76f8b8068a15db1
Minyoung Go <hedone21@gmail.com>
2018-07-05 09:48:32 +0900

    [Modified] Make install.sh guide looks better
    Before, install.sh is not printed good. So, It is fixed in this commit.

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ mkdir ~/.govimrc
 cp -r . ~/.govimrc/
 ln -s ~/.govimrc/vimrc ~/.vimrc
 
-echo "1. Run \"PlugInstall\" on vim command mode(\':\')"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "1. Run \"PlugInstall\" on vim command mode(\":\")"
 echo "2. Run below scripts for sourcecode autocompletion (youcompleteme)"
-echo "\tcd ~/.vim/plugged/youcompleteme"
-echo "\t./install.py --all"
+echo "   cd ~/.vim/plugged/youcompleteme"
+echo "   ./install.py"
 echo "3. Now you can enjoy vim :)"
